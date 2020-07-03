6c2e8f528b5604f42326a911cb044e5e4fbce623
masriant <ssostiami@gmail.com>
2020-07-03 10:45:53 +0700

    memasang template binari admin

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,10 +1,70 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$data = [
+			'title' => 'Home',
+			'isi' => 'v_home'
+		];
+		echo view('layout/v_wrapper', $data);
+	}
+
+	//--------------------------------------------------------------------
+	public function halaman2()
+	{
+		$data = [
+			'title' => 'Halaman 2',
+			'isi' => 'v_halaman2'
+		];
+		echo view('layout/v_wrapper', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function halaman3()
+	{
+		$data = [
+			'title' => 'Halaman 3',
+			'isi' => 'v_halaman3'
+		];
+		echo view('layout/v_wrapper', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function halaman4()
+	{
+		$data = [
+			'title' => 'Halaman 4',
+			'isi' => 'v_halaman4'
+		];
+		echo view('layout/v_wrapper', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function halaman5()
+	{
+		$data = [
+			'title' => 'Halaman 5',
+			'isi' => 'v_halaman5'
+		];
+		echo view('layout/v_wrapper', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function halaman6()
+	{
+		$data = [
+			'title' => 'Halaman 6',
+			'isi' => 'v_halaman6'
+		];
+		echo view('layout/v_wrapper', $data);
 	}
 
 	//--------------------------------------------------------------------
---
+++ app/Views/layout/v_nav.php
@@ -0,0 +1,45 @@
+<!-- /. NAV TOP  -->
+<nav class="navbar-default navbar-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav" id="main-menu">
+            <li class="text-center">
+                <img src="<?= base_url() ?>/template/assets/img/find_user.png" class="user-image img-responsive" />
+            </li>
+
+
+            <li>
+                <a href="<?= base_url() ?>"><i class="fa fa-dashboard"></i> Home</a>
+            </li>
+            <li>
+                <a href="<?= base_url('home/halaman2') ?>"><i class="fa fa-desktop"></i> Halaman 2</a>
+            </li>
+            <li>
+                <a href="<?= base_url('home/halaman3') ?>"><i class="fa fa-qrcode"></i> Halaman 3</a>
+            </li>
+            <li>
+                <a href="<?= base_url('home/halaman4') ?>"><i class="fa fa-bar-chart-o"></i> Halaman 4</a>
+            </li>
+            <li>
+                <a href="<?= base_url('home/halaman5') ?>"><i class="fa fa-table"></i> Halaman 5</a>
+            </li>
+            <li>
+                <a href="<?= base_url('home/halaman6') ?>"><i class="fa fa-edit"></i> Forms </a>
+            </li>
+
+
+
+        </ul>
+
+    </div>
+
+</nav>
+<!-- /. NAV SIDE  -->
+<div id="page-wrapper">
+    <div id="page-inner">
+        <div class="row">
+            <div class="col-md-12">
+                <h2><?= $title ?></h2>
+            </div>
+        </div>
+        <!-- /. ROW  -->
+        <hr />
