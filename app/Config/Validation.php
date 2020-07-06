9eb55e76476aaf5ba4871919d6120172da00ea67
masriant <ssostiami@gmail.com>
2020-07-06 19:34:00 +0700

    edit validasi

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -37,59 +37,31 @@ class Validation
 	//--------------------------------------------------------------------
 	// Validasi input Daftar
 	public $daftar = [
-		'nrp' => [
-			'rules' => 'required|is_unique[tbl_peserta.nrp]|min_length[4]',
-			'errors' => [
-				'required' => 'NRP wajib diisi dan tidak boleh kosong',
-				'is_unique' => 'NRP sudah terdaftar, Input NRP lain',
-				'min_length' => 'Min NRP 4 karakter'
+		'nrp' => 'required|is_unique[tbl_peserta.nrp]|min_length[4]',
+		'nama' => 'required|max_length[20]',
+		'email' => 'required|valid_email',
+		'photo' => 'uploaded[photo]|mime_in[photo,image/jpg,image/jpeg,image/png,image/gif]|max_size[photo,5000]',
+	];
 
-			]
+	public $daftar_errors = [
+		'nrp' => [
+			'required' => 'NRP wajib diisi dan tidak boleh kosong',
+			'is_unique' => 'NRP sudah terdaftar, Input NRP lain',
+			'min_length' => 'Min NRP 4 karakter'
 		],
 		'nama' => [
-			'rules' => 'required|max_length[20]',
-			'errors' => [
-				'required' => 'Nama wajib diisi dan tidak boleh kosong',
-				'max_length' => 'max 20 Karakter'
-
-			]
+			'required' => 'Nama wajib diisi dan tidak boleh kosong',
+			'max_length' => 'max 20 Karakter'
 		],
 		'email' => [
-			'rules' => 'required|valid_email',
-			'errors' => [
-				'required' => 'Email wajib diisi dan tidak boleh kosong',
-				'valid_email' => 'Email tidak valid'
-
-			]
+			'required' => 'Email wajib diisi dan tidak boleh kosong',
+			'valid_email' => 'Email tidak valid'
 		],
 		'photo' => [
-			'rules' => 'uploaded[photo]|mime_in[photo,image/jpg,image/jpeg,image/png,image/gif]|max_size[photo,5000]',
-			'errors' => [
-				'uploaded' => 'Photo wajib diisi dan tidak boleh kosong',
-				'max_size' => 'Maximal ukuran 5MB'
-
-			]
+			'max_size' => 'Maximal ukuran 5MB',
+			'uploaded' => 'Photo wajib diisi dan tidak boleh kosong'
 		]
 	];
-	// public $daftar_errors = [
-	// 	'nrp' => [
-	// 		'required' => 'NRP wajib diisi dan tidak boleh kosong',
-	// 		'is_unique' => 'NRP sudah terdaftar, Input NRP lain',
-	// 		'min_length' => 'Min NRP 4 karakter'
-	// 	],
-	// 	'nama' => [
-	// 		'required' => 'Nama wajib diisi dan tidak boleh kosong',
-	// 		'max_length' => 'max 20 Karakter'
-	// 	],
-	// 	'email' => [
-	// 		'required' => 'Email wajib diisi dan tidak boleh kosong',
-	// 		'valid_email' => 'Email tidak valid'
-	// 	],
-	// 	'photo' => [
-	// 		'max_size' => 'Maximal ukuran 5MB',
-	// 		'uploaded' => 'Photo wajib diisi dan tidak boleh kosong'
-	// 	]
-	// ];
 	//--------------------------------------------------------------------
 
 }
